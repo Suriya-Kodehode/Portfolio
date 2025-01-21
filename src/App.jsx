59c6e2c02a629aff9c1d5c7cab3d7a5ef0147d62
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-01-21 14:22:32 +0100

    update 0.1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 
+import { Header } from './assets/Header/header.jsx'
 import { ToggleMode } from './assets/toggleTheme/toggleMode.jsx'
 import { Content } from './assets/Content/content.jsx'
 
@@ -9,11 +10,18 @@ function App() {
 
   return (
     <div id='appContainer'>
-      <div className='header'><ToggleMode /></div>
+      <div className='header'>
+        <ToggleMode />
+        <Header />
+      </div>
       
-      <div className='main'><Content /></div>
+      <div className='main'>
+        <Content />
+        </div>
       
-      <div className='footer'></div>
+      <div className='footer'>
+
+      </div>
     </div>
   )
 }
