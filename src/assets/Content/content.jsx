233341c1d8d6c9054cb41ea330322482ff410d1c
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-01-24 14:55:47 +0100

    add image

--- src/assets/Content/content.jsx
+++ src/assets/Content/content.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 import styles from './content.module.css';
 
+
 export function Content() {
 
 
 
     return (
         <>
-            
+            <main className={styles.content}>
+                <div className={styles.imgArea}>
+                    <img src='/images/Me2024.jpg' alt='Me in 2024'/>
+                </div>
+            </main>
         </>
     )
 }
