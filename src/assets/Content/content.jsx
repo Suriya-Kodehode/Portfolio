c06c8a9fa39a8f424a63763ec184100574bc12d2
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-01-28 14:49:36 +0100

    14:49

--- src/assets/Content/content.jsx
+++ src/assets/Content/content.jsx
@@ -16,12 +16,12 @@ export function Content() {
                             <div className={styles.imgContainer}>
                                 <img src='/images/Me2024.jpg' alt='Me in 2024'/>
                             </div>
+                        </div>
+                        <div className={styles.myContact}>
                             <div className={styles.imgText}>
                                 <h2>Suriya Suntiviraporn</h2>
                                 <p>Hei! jeg heter Suriya,  en webdeveloper med omtrent 6 måneder erfering fra kurset ved Jobbloop hvor jeg har lært JavaScript, HTML, CSS og Figma. </p>
                             </div>
-                        </div>
-                        <div className={styles.myContact}>
                             <h3>Kontakt info</h3>
                             <div className={styles.infoLink}>
                                 <a href='mailto:suriya.kodehode@gmail.com'>suriya.kodehode@gmail.com</a>
