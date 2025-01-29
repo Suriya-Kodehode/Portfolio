b6d989d06624889e6f6bd5636287aeb6c16a8163
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-01-29 12:21:46 +0100

    Vite version update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './assets/Components/toggleTheme/ThemeContext.jsx'
 
 import Home from './assets/Pages/Home/Home.jsx'
 import Ferdighet from './assets/Pages/Ferdighet/Ferdighet.jsx'
@@ -9,12 +10,14 @@ function App() {
   
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/ferdighet' element={<Ferdighet/>}/>
-      </Routes>
-    </Router>
+    <ThemeProvider>
+        <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/ferdighet' element={<Ferdighet/>}/>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
