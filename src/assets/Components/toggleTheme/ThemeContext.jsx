9957f05b4c3b7573c97051d4ddbb7731b81691a1
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-02-02 23:31:47 +0100

    checking for error

--- src/assets/Components/toggleTheme/ThemeContext.jsx
+++ src/assets/Components/toggleTheme/ThemeContext.jsx
@@ -5,11 +5,20 @@ export const ThemeContext = createContext()
 export const ThemeProvider = ({children}) => {
     const [theme, setTheme] = useState(() => {
         const savedTheme = localStorage.getItem('theme')
+
+        // console.log('Saved theme:', savedTheme)
+
         if(savedTheme) return savedTheme
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+        // console.log('Theme preferred:', preferredTheme)
+
+        return preferredTheme ? 'dark' : 'light';
     })
 
     useEffect(() => {
+        // console.log('Setting theme:', theme)
+
        document.documentElement.setAttribute('data-theme', theme)
        localStorage.setItem('theme', theme)
     }, [theme]);
